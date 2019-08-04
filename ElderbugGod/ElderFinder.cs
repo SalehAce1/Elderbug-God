dd7a97815ea486353139f488aa0a702f04228dcc
Mohammad Ghaemi <seyedmohammadghaem@gmail.com>
2019-08-04 17:24:38 -0400

    Elderbug shoots lasers

---
+++ ElderbugGod/ElderFinder.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Modding;
+using ModCommon;
+using System.Collections;
+using USceneManager = UnityEngine.SceneManagement.SceneManager;
+using Logger = Modding.Logger;
+using UnityEngine.SceneManagement;
+
+namespace ElderbugGod
+{
+    class ElderFinder : MonoBehaviour
+    {
+        public static bool died;
+        private GameObject _target;
+        private void Start()
+        {
+            _target = HeroController.instance.gameObject;
+            USceneManager.activeSceneChanged += SceneChanged;
+            ModHooks.Instance.AfterPlayerDeadHook += AfterPlayerDied;
+            Log("wow we be here");
+            ElderbugGod.preloadedGO["beam"].GetComponent<tk2dSprite>().GetCurrentSpriteDef().material.mainTexture = ElderbugGod.SPRITES[0].texture;
+            ElderbugGod.preloadedGO["ball"].GetComponent<tk2dSprite>().GetCurrentSpriteDef().material.mainTexture = ElderbugGod.SPRITES[0].texture;
+            ElderbugGod.preloadedGO["beam"].GetComponent<DamageHero>().damageDealt = 99;
+            Log("wow did it");
+        }
+
+        private void AfterPlayerDied()
+        {
+            died = true;
+        }
+
+        private void SceneChanged(Scene arg0, Scene arg1)
+        {
+            if (arg1.name == "Town" && !died)
+            {
+                GameObject elder = GameObject.Find("Elderbug");
+                StartCoroutine(DelayedBoio(elder));
+            }
+            else if (arg1.name == "Town")
+            {
+                StartCoroutine(AfterDeed());
+            }
+        }
+
+        IEnumerator DelayedBoio(GameObject Elder)
+        {
+            yield return null;
+            yield return new WaitWhile(() => _target.transform.GetPositionX() - Elder.transform.GetPositionX() < 13f);
+            Log("oh oh");
+            while (!died)
+            {
+                GameObject goBeam = Instantiate(ElderbugGod.preloadedGO["beam"]);
+                GameObject goBall = Instantiate(ElderbugGod.preloadedGO["ball"]);
+                goBeam.SetActive(true);
+                goBall.SetActive(true);
+                goBeam.transform.localScale *= 1.7f;
+                goBall.transform.localScale *= 1.7f;
+                var pos2 = new Vector2(Elder.transform.GetPositionX() + 1.7f, Elder.transform.GetPositionY()+1f);
+                var pos1 = HeroController.instance.transform.position;
+                goBeam.transform.SetPosition2D(pos2);
+                goBall.transform.SetPosition2D(pos2);
+                var ang = Mathf.Atan2(pos1.y - pos2.y, pos1.x - pos2.x) * Mathf.Rad2Deg;
+                Log(ang);
+                goBeam.transform.SetRotation2D(ang);
+                goBall.transform.SetRotation2D(ang);
+                //yield return null;//Beam Extender
+                Destroy(goBeam.LocateMyFSM("destroy_if_gameobject_null"));
+                goBeam.GetComponent<tk2dSpriteAnimator>().enabled = true;
+                goBeam.GetComponent<MeshRenderer>().enabled = true;
+                Destroy(goBall.LocateMyFSM("destroy_if_gameobject_null"));
+                goBall.GetComponent<tk2dSpriteAnimator>().enabled = true;
+                goBall.GetComponent<MeshRenderer>().enabled = true;
+                yield return null;
+                goBall.GetComponent<tk2dSpriteAnimator>().Play("Ball Antic");
+                goBeam.GetComponent<tk2dSpriteAnimator>().Play("Beam Antic");
+                yield return new WaitForSeconds(0.7f);
+                goBall.GetComponent<tk2dSpriteAnimator>().Play("Ball Shoot");
+                goBeam.GetComponent<tk2dSpriteAnimator>().Play("Beam Shoot");
+                yield return new WaitForSeconds(0.8f);
+                goBall.GetComponent<tk2dSpriteAnimator>().Play("Ball End");
+                goBeam.GetComponent<tk2dSpriteAnimator>().Play("Beam End");
+                yield return new WaitWhile(() => goBeam.GetComponent<tk2dSpriteAnimator>().IsPlaying("Beam End"));
+                Destroy(goBall);
+                Destroy(goBeam);
+                yield return null;
+            }
+        }
+        IEnumerator AfterDeed()
+        {
+            Log("am here");
+            yield return new WaitWhile(() => !Input.GetKey(KeyCode.Mouse0));
+            GameObject eld = GameObject.Find("Elderbug");
+            Log("click of ma");
+            for (int i = 0; i < 10f; i++)
+            {
+                eld.transform.Rotate(Vector3.forward * -250f * Time.deltaTime);
+                yield return null;
+            }
+            Log("heree");
+            yield return new WaitForSeconds(1f);
+            eld.AddComponent<Rigidbody2D>().gravityScale = 0f;
+            eld.GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Cos(45f) * 30f, Mathf.Sin(45f) * 30f);
+            yield return new WaitForSeconds(0.05f);
+            eld.GetComponent<Rigidbody2D>().velocity = new Vector2(0f,0f);
+            Log("hererere");
+
+            GameObject goBeam = Instantiate(ElderbugGod.preloadedGO["beam"]);
+            GameObject goBall = Instantiate(ElderbugGod.preloadedGO["ball"]);
+            goBeam.SetActive(true);
+            goBall.SetActive(true);
+            goBeam.transform.localScale *= 1.4f;
+            goBall.transform.localScale *= 1.4f;
+            var pos2 = new Vector2(eld.transform.GetPositionX() - 2f, eld.transform.GetPositionY() - 1f);
+            var pos1 = new Vector2(pos2.x-6f,pos2.y-5f);
+            goBeam.transform.SetPosition2D(pos2);
+            goBall.transform.SetPosition2D(pos2);
+            var ang = Mathf.Atan2(pos1.y - pos2.y, pos1.x - pos2.x) * Mathf.Rad2Deg;
+            Log(ang);
+            goBeam.transform.SetRotation2D(ang);
+            goBall.transform.SetRotation2D(ang);
+            //yield return null;//Beam Extender
+            Destroy(goBeam.LocateMyFSM("destroy_if_gameobject_null"));
+            goBeam.GetComponent<tk2dSpriteAnimator>().enabled = true;
+            goBeam.GetComponent<MeshRenderer>().enabled = true;
+            Destroy(goBall.LocateMyFSM("destroy_if_gameobject_null"));
+            goBall.GetComponent<tk2dSpriteAnimator>().enabled = true;
+            goBall.GetComponent<MeshRenderer>().enabled = true;
+            yield return null;
+            goBall.GetComponent<tk2dSpriteAnimator>().Play("Ball Antic");
+            goBeam.GetComponent<tk2dSpriteAnimator>().Play("Beam Antic");
+            yield return new WaitForSeconds(0.7f);
+            goBall.GetComponent<tk2dSpriteAnimator>().Play("Ball Shoot");
+            goBeam.GetComponent<tk2dSpriteAnimator>().Play("Beam Shoot");
+
+            eld.GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Cos(45f) * 30f, Mathf.Sin(45f) * 30f);
+
+            yield return new WaitForSeconds(1f);
+            goBall.GetComponent<tk2dSpriteAnimator>().Play("Ball End");
+            goBeam.GetComponent<tk2dSpriteAnimator>().Play("Beam End");
+            yield return new WaitWhile(() => goBeam.GetComponent<tk2dSpriteAnimator>().IsPlaying("Beam End"));
+            Destroy(goBall);
+            Destroy(goBeam);
+        }
+
+        private static void Log(object obj)
+        {
+            Logger.Log("[GodBug] " + obj);
+        }
+    }
+}
---
+++ ElderbugGod/ElderbugGod.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using Modding;
+using JetBrains.Annotations;
+using ModCommon;
+using MonoMod.RuntimeDetour;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using USceneManager = UnityEngine.SceneManagement.SceneManager;
+using UObject = UnityEngine.Object;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ElderbugGod
+{
+    [UsedImplicitly]
+    public class ElderbugGod : Mod, ITogglableMod
+    {
+        public static Dictionary<string, GameObject> preloadedGO = new Dictionary<string, GameObject>();
+
+        public static ElderbugGod Instance;
+
+        public static readonly List<Sprite> SPRITES = new List<Sprite>();
+
+        public override string GetVersion()
+        {
+            return "0.0.0.0";
+        }
+
+        public override List<(string, string)> GetPreloadNames()
+        {
+            return new List<(string, string)>
+            {
+                ("Mines_18_boss","Beam Ball"),
+                ("Mines_18_boss","Beam"),
+            };
+        }
+
+        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
+        {
+            Log("Storing GOs");
+            preloadedGO.Add("ball", preloadedObjects["Mines_18_boss"]["Beam Ball"]);
+            preloadedGO.Add("beam", preloadedObjects["Mines_18_boss"]["Beam"]);
+            Instance = this;
+            Log("Initalizing.");
+
+            Unload();
+            ModHooks.Instance.AfterSavegameLoadHook += AfterSaveGameLoad;
+            ModHooks.Instance.NewGameHook += AddComponent;
+            ModHooks.Instance.LanguageGetHook += LangGet;
+            int ind = 0;
+            Assembly asm = Assembly.GetExecutingAssembly();
+            //MusicLoad.LoadAssets.LoadWavFile();
+            foreach (string res in asm.GetManifestResourceNames())
+            {
+                if (!res.EndsWith(".png"))
+                {
+                    continue;
+                }
+
+                using (Stream s = asm.GetManifestResourceStream(res))
+                {
+                    if (s == null) continue;
+
+                    byte[] buffer = new byte[s.Length];
+                    s.Read(buffer, 0, buffer.Length);
+                    s.Dispose();
+                    var tex = new Texture2D(1, 1);
+                    tex.LoadImage(buffer, true);
+                    SPRITES.Add(Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f)));
+                    Log("Created sprite from embedded image: " + res + " at ind " + ++ind);
+                }
+            }
+        }
+
+        private string LangGet(string key, string sheettitle)
+        {
+            if (ElderFinder.died)
+            {
+                switch (key)
+                {
+                    case "ELDERBUG_INTRO_WALKEDBY": return "Young buggo, that was an incredibly rude thing you attempted to do, passing an old bug like me, you would have killed me too if I wasn't an NPC.<page>Kids these days have no manners.<page>";
+                    case "ELDERBUG_INTRO_MAIN": return "What I don't understand is why you would come here, to this dead kingdom. I suggest you turn back while you can and head to Pharloom.<page>Now if you will excuse me, I have a tea party with Mr. Mushroom to attend to.";
+                    case "ELDERBUG_MAIN": return "ElderGod";
+                    case "ELDERBUG_SUB": return "Cutest Buggo";
+                    case "LISTEN": return "Hear God's Words";
+                    default: return Language.Language.GetInternal(key, sheettitle);
+                }
+            }
+            else return Language.Language.GetInternal(key, sheettitle);
+        }
+
+        private void AfterSaveGameLoad(SaveGameData data) => AddComponent();
+
+        private void AddComponent()
+        {
+            GameManager.instance.gameObject.AddComponent<ElderFinder>();
+        }
+
+        public void Unload()
+        {
+            AudioListener.volume = 1f;
+            AudioListener.pause = false;
+            ModHooks.Instance.AfterSavegameLoadHook -= AfterSaveGameLoad;
+            ModHooks.Instance.NewGameHook -= AddComponent;
+            ModHooks.Instance.LanguageGetHook -= LangGet;
+
+            // ReSharper disable once Unity.NoNullPropogation
+            //var x = GameManager.instance?.gameObject.GetComponent<ArenaFinder>();
+            //if (x == null) return;
+            //UObject.Destroy(x);
+        }
+    }
+}
